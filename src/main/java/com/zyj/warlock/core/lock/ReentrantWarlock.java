4858717d81e231df63952d67ec834a8041fc9f2a
zhouyijin <zhouyijin@yxt.com>
2023-03-07 15:59:00 +0800

    调整可重入锁的结构

--- src/main/java/com/zyj/warlock/core/lock/ReentrantWarlock.java
+++ src/main/java/com/zyj/warlock/core/lock/ReentrantWarlock.java
@@ -16,8 +16,18 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class ReentrantWarlock implements Warlock {
 
+    /**
+     * 一个锁的池子
+     * 第一次拿, 会初始化这个锁
+     * 每次拿都会在计数器上+1, pair.right += 1
+     * 每次归还都会在计数器上-1, pair.left -= 1
+     * 当计数器归零, 则从内存中移除这个锁
+     */
     private static final ConcurrentHashMap<String, Pair<ReentrantLock, AtomicInteger>> REENTRANT_LOCK_MAP = new ConcurrentHashMap<>();
 
+    /**
+     * 锁的具体信息
+     */
     private final LockInfo lockInfo;
 
     public ReentrantWarlock(LockInfo lockInfo) {
@@ -27,6 +37,22 @@ public class ReentrantWarlock implements Warlock {
     @Override
     public Object doWithLock(BizFunction bizFunc) throws Throwable {
         //1. 拿锁
+        ReentrantLock lock = getLock();
+        //2. 上锁
+        lock.lock();
+        try {
+            //3. 执行业务代码
+            return bizFunc.doBiz();
+        } finally {
+            //4. 解锁
+            lock.unlock();
+            //5. 还锁
+            returnLock();
+        }
+    }
+
+    //根据lockInfo 拿锁
+    private ReentrantLock getLock() {
         Pair<ReentrantLock, AtomicInteger> lockPair = REENTRANT_LOCK_MAP.compute(lockInfo.getLockKey(), (s, pair) -> {
             if (pair == null) {
                 //没有就初始化
@@ -35,23 +61,18 @@ public class ReentrantWarlock implements Warlock {
             pair.getRight().incrementAndGet();
             return pair;
         });
-        //2. 上锁
-        lockPair.getLeft().lock();
-        try {
-            //3. 执行业务代码
-            return bizFunc.doBiz();
-        } finally {
-            //4. 解锁
-            lockPair.getLeft().unlock();
-            //5. 还锁
-            REENTRANT_LOCK_MAP.computeIfPresent(lockInfo.getLockKey(), (s, pair) -> {
-                int holdCount = pair.getRight().decrementAndGet();
-                if (holdCount <= 0) {
-                    //返回null,相当于把这个value给移除了
-                    return null;
-                }
-                return pair;
-            });
-        }
+        return lockPair.getLeft();
+    }
+
+    //根据lockInfo 归还锁
+    private void returnLock() {
+        REENTRANT_LOCK_MAP.computeIfPresent(lockInfo.getLockKey(), (s, pair) -> {
+            int holdCount = pair.getRight().decrementAndGet();
+            if (holdCount <= 0) {
+                //返回null,相当于把这个value给移除了
+                return null;
+            }
+            return pair;
+        });
     }
 }
