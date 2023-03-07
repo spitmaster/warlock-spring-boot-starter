fc7478a11fe95c8d3a146c31f3f4ef02ead04358
zhouyijin <zhouyijin@yxt.com>
2023-03-07 17:57:45 +0800

    构造LockInfo移动到DefaultWarlockFactory中去

--- src/main/java/com/zyj/warlock/core/DefaultWarlockFactory.java
+++ src/main/java/com/zyj/warlock/core/DefaultWarlockFactory.java
@@ -4,7 +4,12 @@ import com.zyj.warlock.annotation.Wlock;
 import com.zyj.warlock.core.lock.ReadWarlock;
 import com.zyj.warlock.core.lock.ReentrantWarlock;
 import com.zyj.warlock.core.lock.WriteWarlock;
+import com.zyj.warlock.util.SpelExpressionUtil;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+
+import java.lang.reflect.Method;
+import java.time.Duration;
 
 /**
  * WarlockFactory的简易实现
@@ -18,7 +23,7 @@ public class DefaultWarlockFactory implements WarlockFactory {
     @Override
     public Warlock build(ProceedingJoinPoint pjp, Wlock wlock) {
         //1. 构造锁
-        LockInfo lockInfo = LockInfo.from(pjp, wlock);
+        LockInfo lockInfo = buildLock(pjp, wlock);
 
         //2. 根据锁类型选择合适的锁
         //According lock type decide what warlock should be used
@@ -38,6 +43,39 @@ public class DefaultWarlockFactory implements WarlockFactory {
                 break;
         }
         return warlock;
+    }
+
+    private LockInfo buildLock(ProceedingJoinPoint pjp, Wlock wlock) {
+        LockInfo lockInfo = new LockInfo();
+        //1. 构造lockKey
+        //收集锁的信息
+        // 获取方法参数值
+        Object[] arguments = pjp.getArgs();
+        // 获取method
+        MethodSignature signature = (MethodSignature) pjp.getSignature();
+        Method method = signature.getMethod();
+
+        String lockName = wlock.name();
+        // 获取spel表达式
+        String keySpEL = wlock.key();
+        String key = SpelExpressionUtil.parseSpel(method, arguments, keySpEL, String.class);
+
+        /*
+         * construct a lockkey that indicate a unique lock
+         * this lock would be used in Warlock.beforeBiz and Warlock.afterBiz and Warlock.except
+         */
+        String lockKey = lockName + key;
+        lockInfo.setLockKey(lockKey);
+        //2. 拿到lockType
+        lockInfo.setLockType(wlock.lockType());
+        //3. 获取等待时间
+        Duration waitTime = Duration.of(wlock.waiting().waitTime(), wlock.waiting().timeUnit().toChronoUnit());
+        lockInfo.setWaitTime(waitTime);
+        //4. 获取等待时间
+        Duration leaseTime = Duration.of(wlock.leasing().leaseTime(), wlock.leasing().timeUnit().toChronoUnit());
+        lockInfo.setLeaseTime(leaseTime);
+        //5. 返回锁信息
+        return lockInfo;
     }
 
 
--- src/main/java/com/zyj/warlock/core/LockInfo.java
+++ src/main/java/com/zyj/warlock/core/LockInfo.java
@@ -1,14 +1,9 @@
 package com.zyj.warlock.core;
 
-import com.zyj.warlock.annotation.Wlock;
 import com.zyj.warlock.enums.LockType;
 import com.zyj.warlock.handler.WaitTimeoutHandler;
-import com.zyj.warlock.util.SpelExpressionUtil;
 import lombok.Data;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.reflect.MethodSignature;
 
-import java.lang.reflect.Method;
 import java.time.Duration;
 
 /**
@@ -57,44 +52,4 @@ public class LockInfo {
      * @see com.zyj.warlock.annotation.Leasing
      */
     private Duration leaseTime;
-
-    /**
-     * 通过切点和注解中的信息, 构造锁的信息
-     *
-     * @param pjp   切点
-     * @param wlock 注解
-     * @return 锁信息
-     */
-    public static LockInfo from(ProceedingJoinPoint pjp, Wlock wlock) {
-        LockInfo lockInfo = new LockInfo();
-        //1. 构造lockKey
-        //收集锁的信息
-        // 获取方法参数值
-        Object[] arguments = pjp.getArgs();
-        // 获取method
-        MethodSignature signature = (MethodSignature) pjp.getSignature();
-        Method method = signature.getMethod();
-
-        String lockName = wlock.name();
-        // 获取spel表达式
-        String keySpEL = wlock.key();
-        String key = SpelExpressionUtil.parseSpel(method, arguments, keySpEL, String.class);
-
-        /*
-         * construct a lockkey that indicate a unique lock
-         * this lock would be used in Warlock.beforeBiz and Warlock.afterBiz and Warlock.except
-         */
-        String lockKey = lockName + key;
-        lockInfo.setLockKey(lockKey);
-        //2. 拿到lockType
-        lockInfo.setLockType(wlock.lockType());
-        //3. 获取等待时间
-        Duration waitTime = Duration.of(wlock.waiting().waitTime(), wlock.waiting().timeUnit().toChronoUnit());
-        lockInfo.setWaitTime(waitTime);
-        //4. 获取等待时间
-        Duration leaseTime = Duration.of(wlock.leasing().leaseTime(), wlock.leasing().timeUnit().toChronoUnit());
-        lockInfo.setLeaseTime(leaseTime);
-        //5. 返回锁信息
-        return lockInfo;
-    }
 }
