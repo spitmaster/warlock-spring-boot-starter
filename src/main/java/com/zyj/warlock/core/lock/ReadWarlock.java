c83377189dfb28dad5fc240b8e63a25d5535019a
zhouyijin <zhouyijin@yxt.com>
2023-03-07 14:11:43 +0800

    基于JUC的读写锁实现

--- src/main/java/com/zyj/warlock/core/lock/ReadWarlock.java
+++ src/main/java/com/zyj/warlock/core/lock/ReadWarlock.java
@@ -3,6 +3,12 @@ package com.zyj.warlock.core.lock;
 import com.zyj.warlock.core.LockInfo;
 import com.zyj.warlock.core.Warlock;
 
+/**
+ * 基于JUC实现的读锁
+ *
+ * @author zhouyijin
+ * @see WriteWarlock 与 WriteWarlock 共同使用一个Map管理锁
+ */
 public class ReadWarlock implements Warlock {
 
     private final LockInfo lockInfo;
--- src/main/java/com/zyj/warlock/core/lock/WriteWarlock.java
+++ src/main/java/com/zyj/warlock/core/lock/WriteWarlock.java
@@ -2,8 +2,26 @@ package com.zyj.warlock.core.lock;
 
 import com.zyj.warlock.core.LockInfo;
 import com.zyj.warlock.core.Warlock;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * 基于JUC实现的写锁
+ *
+ * @author zhouyijin
+ */
 public class WriteWarlock implements Warlock {
+
+    /**
+     * ReadWarlock 与 WriteWarlock 共用同一个Map进行锁的管理
+     *
+     * @see ReadWarlock 与 ReadWarlock 共用一个Map管理锁
+     */
+    static final ConcurrentHashMap<String, Pair<ReentrantReadWriteLock, AtomicInteger>> READ_WRITE_LOCK_MAP = new ConcurrentHashMap<>();
 
     private final LockInfo lockInfo;
 
@@ -13,6 +31,32 @@ public class WriteWarlock implements Warlock {
 
     @Override
     public Object doWithLock(BizFunction bizFunc) throws Throwable {
-        return null;
+        //1. 拿锁
+        Pair<ReentrantReadWriteLock, AtomicInteger> lockPair = READ_WRITE_LOCK_MAP.compute(lockInfo.getLockKey(), (s, pair) -> {
+            if (pair == null) {
+                //没有就初始化
+                pair = new ImmutablePair<>(new ReentrantReadWriteLock(), new AtomicInteger(0));
+            }
+            pair.getRight().incrementAndGet();
+            return pair;
+        });
+        //2. 上锁
+        lockPair.getLeft().writeLock().lock();
+        try {
+            //3. 执行业务代码
+            return bizFunc.doBiz();
+        } finally {
+            //4. 解锁
+            lockPair.getLeft().writeLock().lock();
+            //5. 还锁
+            READ_WRITE_LOCK_MAP.computeIfPresent(lockInfo.getLockKey(), (s, pair) -> {
+                int holdCount = pair.getRight().decrementAndGet();
+                if (holdCount <= 0) {
+                    //返回null,相当于把这个value给移除了
+                    return null;
+                }
+                return pair;
+            });
+        }
     }
 }
