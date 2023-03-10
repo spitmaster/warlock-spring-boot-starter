cbb3b41e646d0f1700c213ba9b143a362e135b45
zhouyijin <zhouyijin@yxt.com>
2023-03-10 18:18:05 +0800

    单机的CyclicBarrier实现

--- src/main/java/io/github/spitmaster/warlock/core/barrier/StandaloneWBarrier.java
+++ src/main/java/io/github/spitmaster/warlock/core/barrier/StandaloneWBarrier.java
@@ -1,6 +1,13 @@
 package io.github.spitmaster.warlock.core.barrier;
 
+import io.github.spitmaster.warlock.util.JoinPointUtil;
+import org.apache.commons.lang3.tuple.Pair;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 基于JUC的CyclicBarrier实现
@@ -8,6 +15,10 @@ import org.aspectj.lang.ProceedingJoinPoint;
  * @author zhouyijin
  */
 public class StandaloneWBarrier implements WBarrier {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StandaloneWBarrier.class);
+
+    private static final ConcurrentHashMap<String, Pair<CyclicBarrier, AtomicInteger>> CYCLIC_BARRIER_MAP = new ConcurrentHashMap<>();
 
     private final BarrierInfo barrierInfo;
 
@@ -17,7 +28,65 @@ public class StandaloneWBarrier implements WBarrier {
 
     @Override
     public Object doWithBarrier(ProceedingJoinPoint pjp) throws Throwable {
-        return null;
+        //1. 获取这个key对应的 cyclicBarrier
+        CyclicBarrier cyclicBarrier = this.getCyclicBarrier();
+        try {
+            //2. 等待超时, 或者其他同时在等的线程超时, 那么这个 cyclicBarrier 就会 Broken
+            cyclicBarrier.await(barrierInfo.getWaitTime().toMillis(), TimeUnit.MILLISECONDS);
+        } catch (InterruptedException | BrokenBarrierException | TimeoutException e) {
+            //3. 修复 cyclicBarrier, 并进行超时处理
+            //一旦Broken这个CyclicBarrier就相当于废了, 要立刻恢复,
+            // 但是原来的 cyclicBarrier 实际上已经不能继续用了, 因为还有许多其他线程在不停的处理Exception,
+            // 那么这时候就需要将Map中的换掉, 重新初始化
+            return processBrokenBarrier(pjp, cyclicBarrier, e);
+        }
+        return pjp.proceed();
+    }
+
+    private Object processBrokenBarrier(ProceedingJoinPoint pjp, CyclicBarrier cyclicBarrier, Exception e) throws Throwable {
+        LOGGER.error("processBrokenBarrier; method={}", JoinPointUtil.methodName(pjp), e);
+        //使用同一个Barrier的需要一起处理, 不能各处理各的
+        synchronized (cyclicBarrier) {
+            CyclicBarrier newCyclicBarrier = this.getCyclicBarrier();
+            if (cyclicBarrier == newCyclicBarrier) {
+                //说明本线程是第一个进入 synchronized 代码块的线程
+                //需要将已有的 barrier废弃掉, 其他线程再进来, 他们会自己初始化
+                this.returnCyclicBarrier();
+            }
+        }
+        //超时处理
+        return this.barrierInfo.getWaitTimeoutHandler().handleWaitTimeout(pjp);
+    }
+
+    /**
+     * 根据key获取CyclicBarrier, 懒加载
+     *
+     * @return CyclicBarrier
+     */
+    protected CyclicBarrier getCyclicBarrier() {
+        Pair<CyclicBarrier, AtomicInteger> barrierPair = CYCLIC_BARRIER_MAP.compute(barrierInfo.getBarrierKey(), (s, pair) -> {
+            if (pair == null) {
+                //没有就初始化
+                pair = Pair.of(new CyclicBarrier(barrierInfo.getParties()), new AtomicInteger(0));
+            }
+            pair.getRight().incrementAndGet();
+            return pair;
+        });
+        return barrierPair.getLeft();
+    }
+
+    /**
+     * 销毁map中的 CyclicBarrier
+     */
+    protected void returnCyclicBarrier() {
+        CYCLIC_BARRIER_MAP.computeIfPresent(barrierInfo.getBarrierKey(), (s, pair) -> {
+            int holdCount = pair.getRight().decrementAndGet();
+            if (holdCount <= 0) {
+                //返回null,相当于把这个信号量删除了
+                return null;
+            }
+            return pair;
+        });
     }
 
     @Override
--- src/test/java/io/github/spitmaster/warlock/sample/MyCyclicBarrierTest.java
+++ src/test/java/io/github/spitmaster/warlock/sample/MyCyclicBarrierTest.java
@@ -3,6 +3,7 @@ package io.github.spitmaster.warlock.sample;
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class MyCyclicBarrierTest {
     public static void main(String[] args) throws InterruptedException {
@@ -12,11 +13,10 @@ public class MyCyclicBarrierTest {
             TimeUnit.SECONDS.sleep(1);
             new Thread(() -> {
                 try {
-                    cyclicBarrier.await();
+                    cyclicBarrier.await(1, TimeUnit.SECONDS);
                     System.out.println("print : " + finalI);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                } catch (BrokenBarrierException e) {
+                } catch (InterruptedException | BrokenBarrierException | TimeoutException e) {
+                    cyclicBarrier.reset();
                     throw new RuntimeException(e);
                 }
             }).start();
