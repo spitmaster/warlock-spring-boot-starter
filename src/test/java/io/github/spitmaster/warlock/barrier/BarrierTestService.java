3a9d80b525ad67c737e1ca00127b35f3852784e5
zhouyijin <zhouyijin@yxt.com>
2023-03-21 23:18:16 +0800

    @WcyclicBarrier的单元测试

---
+++ src/test/java/io/github/spitmaster/warlock/barrier/BarrierTestService.java
@@ -0,0 +1,38 @@
+package io.github.spitmaster.warlock.barrier;
+
+import io.github.spitmaster.warlock.annotation.Waiting;
+import io.github.spitmaster.warlock.annotation.WcyclicBarrier;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+public class BarrierTestService {
+
+    AtomicInteger counter1 = new AtomicInteger(0);
+    AtomicInteger counter2 = new AtomicInteger(0);
+
+    public void init() {
+        counter1.set(0);
+        counter2.set(0);
+    }
+
+    @WcyclicBarrier(
+            parties = 2,
+            name = "add1",
+            waiting = @Waiting(waitTime = 1, timeUnit = TimeUnit.SECONDS)
+    )
+    public void add1() {
+        counter1.incrementAndGet();
+        counter2.incrementAndGet();
+    }
+
+    public AtomicInteger getCounter1() {
+        return counter1;
+    }
+
+    public AtomicInteger getCounter2() {
+        return counter2;
+    }
+}
---
+++ src/test/java/io/github/spitmaster/warlock/barrier/StandaloneWbarrierTest.java
@@ -0,0 +1,71 @@
+package io.github.spitmaster.warlock.barrier;
+
+import io.github.spitmaster.warlock.Application;
+import io.github.spitmaster.warlock.exceptions.WarlockException;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+@SpringBootTest(classes = Application.class)
+public class StandaloneWbarrierTest {
+
+    @Autowired
+    private BarrierTestService barrierTestService;
+
+    private static ExecutorService executorService;
+
+    @BeforeAll
+    static void setupAll() {
+        executorService = Executors.newFixedThreadPool(10);
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        executorService.shutdown();
+        executorService = null;
+    }
+
+    @BeforeEach
+    void setup() {
+        barrierTestService.init();
+    }
+
+    @Test
+    void tryWaitTimeout() throws InterruptedException, ExecutionException {
+        List<Callable<Integer>> tasks = new ArrayList<>();
+        //造一个场景只有6个能执行成功, 第七个会timeout
+        for (int i = 0; i < 7; i++) {
+            tasks.add(() -> {
+                try {
+                    //会有6次是成功执行
+                    //1次是超时,然后抛出异常
+                    barrierTestService.add1();
+                } catch (WarlockException e) {
+                    System.out.println(e);
+                }
+                return 1;
+            });
+        }
+        var startTime = System.currentTimeMillis();
+        List<Future<Integer>> futures = executorService.invokeAll(tasks);
+        for (Future<Integer> future : futures) {
+            future.get();
+        }
+        var endTime = System.currentTimeMillis();
+        var consumedTime = endTime - startTime;//执行6个任务耗费的时间
+        System.out.println("consumedTime = " + consumedTime);
+        var counter1 = barrierTestService.getCounter1().get();
+        var counter2 = barrierTestService.getCounter2().get();
+        System.out.println("counter1 = " + counter1);
+        System.out.println("counter2 = " + counter2);
+        Assertions.assertEquals(6, counter1);
+        Assertions.assertEquals(6, counter2);
+        //等待时间超过1秒钟, 因为 @WcyclicBarrier 设置了1秒超时
+        Assertions.assertTrue(consumedTime >= 1000 && consumedTime <= 1500);
+    }
+
+}
